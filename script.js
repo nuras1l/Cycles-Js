b27e706e7b8732286c298cc37a69b6db3d14f1f0
nuras1l <nurasylmukhtargazy@gmail.com>
2025-07-18 11:22:15 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,83 @@
+//1
+let count = 0
+while (count < 10) {
+    count++
+    console.log(count)
+}
+
+//2
+let count2 = 0
+while (count2 < 10) {
+    count2++
+    if (count2 === 5) {
+        continue
+    }
+    if (count2 == 8) {
+        break
+    }
+    console.log(count2)
+
+}
+
+//3
+let count3 = 0
+do {
+    count3++;
+    console.log(count3);
+} while (count3 < 5)
+
+//4
+
+while (true) {
+    const input = prompt("Введите число (введите 10 для выхода):");
+    
+    if (Number(input) === 10) {
+      console.log("Вы ввели 10. Цикл завершен.");
+      break;
+    }
+  
+    console.log("Вы ввели:", input, "- Попробуйте снова.");
+  }
+  
+
+//5
+for(let i = 1;i<11;i++){
+    console.log(i)
+}
+
+//6
+for (let i = 1 ;; ){
+    if(i>100){
+        break
+    };
+    console.log(i);
+    i++;
+}
+
+//7
+let numbers1 = [1,2,3,4,5]
+for (let i = 0; i < numbers1.length; i++) {
+    console.log(numbers1[i]);
+  }
+  
+//9
+let numbers2 =[]
+for ( let i = 0;;){
+    i++
+    numbers2.push(i)
+    if (i==10){
+        break
+    }
+}
+console.log(numbers2.reverse())
+
+//10
+for (let i = 1;i<=10;i++){
+    if (i%2==0){
+        console.log(i)
+    }
+    else{
+        continue
+    }
+    
+}
